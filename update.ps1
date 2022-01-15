06736fdffcc673046500d31b506a13d91635d58a
Sebastian Pech <windows@spech.de>
2022-01-15 10:16:14 +0100

    Prepare tooling

---
+++ update.ps1
@@ -0,0 +1,17 @@
+$pkgold = Get-Content "package-lock.json" | Out-String | ConvertFrom-Json
+$bversionold = $pkgold.dependencies.bootstrap.version
+Write-Host "Bootstrap current: $bversionold"
+
+npm update
+
+Copy-Item -Force .\node_modules\bootstrap\dist\js\bootstrap.bundle.min.js .\assets\js\bootstrap.bundle.min.js
+
+New-Item -Path ".\assets\sass\bootstrap\" -ItemType "Directory" -Force
+Copy-Item -Path ".\node_modules\bootstrap\scss\*" -Destination ".\assets\sass\bootstrap\" -Recurse -Force
+
+$pkg = Get-Content "package-lock.json" | Out-String | ConvertFrom-Json
+$bversion = $pkg.dependencies.bootstrap.version
+Write-Host "Bootstrap: $bversion"
+Write-Host "For tagging:"
+Write-Host "git tag -a v$bversion -m ""Bootstrap version $bversion"""
+Write-Host "git push origin v$bversion"
