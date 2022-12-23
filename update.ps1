40de84a7c2ffc9f36ef142fa2f85b9c331965711
Sebastian Pech <spech@spech.de>
2022-12-23 08:55:08 +0100

    Bootstrap v5.2.3

--- update.ps1
+++ update.ps1
@@ -9,7 +9,7 @@ Copy-Item -Force .\node_modules\bootstrap\dist\js\bootstrap.bundle.min.js .\asse
 New-Item -Path ".\assets\sass\bootstrap\" -ItemType "Directory" -Force
 Copy-Item -Path ".\node_modules\bootstrap\scss\*" -Destination ".\assets\sass\bootstrap\" -Recurse -Force
 
-$pkg = Get-Content "package-lock.json" | Out-String | ConvertFrom-Json
+$pkg = Get-Content "package-lock.json" | Out-String | ConvertFrom-Json -AsHashtable
 $bversion = $pkg.dependencies.bootstrap.version
 Write-Host "Bootstrap: $bversion"
 Write-Host "For tagging:"
